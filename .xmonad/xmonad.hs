34bea096d11c2e3f4a3ebd07d2183e0a095b376b
Matt Russell <matthew.russell@horizon5.org>
2014-10-19 23:58:24 +0100

    Xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,32 @@
+import System.IO
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+ 
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+             { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                            <+> manageHook defaultConfig
+             , layoutHook = avoidStruts  $  layoutHook defaultConfig
+             , logHook = dynamicLogWithPP xmobarPP
+                         { ppOutput = hPutStrLn xmproc
+                         , ppTitle = xmobarColor "aqua" "" . shorten 50
+                          }
+             , modMask = mod4Mask
+             , terminal = "uxterm"
+             }
+             `additionalKeysP`
+             [ ("M-e", spawn "emacsclient -c")
+             , ("M-f", spawn "firefox")
+	     , ("M-m", spawn "thunderbird")
+             ]
+
+
