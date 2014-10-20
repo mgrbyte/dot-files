2a8d761ce151a50feb1e7759bf30656f41bbf0ff
Matthew Russell <matthew.russell@horizon5.org>
2014-10-20 10:12:02 +0100

    Emacs integration.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+
+# setup dot files
 ARCHIVE="/tmp/dot-files.tar.gz"
 git archive -o "$ARCHIVE" HEAD
 cd "$HOME"
 tar xvf "$ARCHIVE"
 rm "$ARCHIVE"
+
+# Ensure emacs is installed properly
+cd "$HOME/.emacs.d"
+git remote -v | grep "netsight/emacs"
+cd "$HOME"
+if [ $? -ne 0 ]; then
+    cd "$HOME"
+    git clone https://github.com/netsight/emacs.d ~/.emacs.d
+    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+    cd ~/.emacs.d
+    cask update
+    cask upgrade
+fi
 
