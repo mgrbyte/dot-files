70f787d48d2b8061b83952cc5f4d64d4e0bbbf6f
Matt Russell <matthew.russell@horizon5.org>
2014-10-20 08:38:46 +0100

    Volume XKeys.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,24 +9,26 @@ myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
     ]
- 
+
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
              { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
-                            <+> manageHook defaultConfig
-             , layoutHook = avoidStruts  $  layoutHook defaultConfig
-             , logHook = dynamicLogWithPP xmobarPP
-                         { ppOutput = hPutStrLn xmproc
-                         , ppTitle = xmobarColor "aqua" "" . shorten 50
-                          }
-             , modMask = mod4Mask
-             , terminal = "uxterm"
-             }
-             `additionalKeysP`
-             [ ("M-e", spawn "emacsclient -c")
-             , ("M-f", spawn "firefox")
-	     , ("M-m", spawn "thunderbird")
+                            <+> manageHook defaultConfig,
+               layoutHook = avoidStruts  $  layoutHook defaultConfig,
+               logHook = dynamicLogWithPP xmobarPP
+                         { ppOutput = hPutStrLn xmproc,
+                           ppTitle = xmobarColor "aqua" "" . shorten 50
+                          },
+               modMask = mod4Mask,
+               terminal = "uxterm"
+	     } `additionalKeysP`
+             [ ("M-e", spawn "emacsclient -c"),
+               ("M-f", spawn "firefox"),
+	       ("M-m", spawn "thunderbird"),
+	       ((0, 0x1008FF11), spawn "amixer set Master 2-"),
+	       ((0, 0x1008FF13), spawn "amixer set Master 2+"),
+	       ((0, 0x1008FF12), spawn "amixer set Master toggle")
              ]
 
 
