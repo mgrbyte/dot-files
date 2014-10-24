9b0c18f7e8e6990cfc137cf98a34004dcb327d37
Matt Russell <matthew.russell@horizon5.org>
2014-10-24 17:35:19 +0100

    Updated emacs customs.

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -11,6 +11,7 @@
 (require 'use-package)
 (require 'org-install)
 (require 'epresent)
+(require 'jabber)
 (use-package ace-jump-mode)
 (use-package ws-butler)
 
@@ -37,19 +38,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+    '(ansi-color-names-vector
+         ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(compilation-message-face (quote default))
  '(cua-global-mark-cursor-color "#2aa198")
  '(cua-normal-cursor-color "#657b83")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
  '(custom-enabled-themes (quote (warm-night)))
- '(custom-safe-themes (quote ("7b0433e99dad500efbdf57cf74553499cde4faf2908a2852850c04b216c41cc9" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "55e0b2d72ea7eec555e36242e4b5008d809504767d1f261d78c28b1245da95a7" "9f403454579affa3abd21364e97d5224a318df342f8fe8af95138996d478f8cb" "2b584cee821f2646c4977eb8061a2a75a4e6c57d211220cf14f4e92960393e4e" default)))
+    '(custom-safe-themes
+         (quote
+             ("f7cbccc9c4bd2d684ff448a2b277b5aff3a837312acc723c5e7b388133f4e480" "7b0433e99dad500efbdf57cf74553499cde4faf2908a2852850c04b216c41cc9" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "55e0b2d72ea7eec555e36242e4b5008d809504767d1f261d78c28b1245da95a7" "9f403454579affa3abd21364e97d5224a318df342f8fe8af95138996d478f8cb" "2b584cee821f2646c4977eb8061a2a75a4e6c57d211220cf14f4e92960393e4e" default)))
  '(fci-rule-color "#eee8d5")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors (--map (solarized-color-blend it "#fdf6e3" 0.25) (quote ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+    '(highlight-symbol-colors
+         (--map
+             (solarized-color-blend it "#fdf6e3" 0.25)
+             (quote
+                 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
  '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors (quote (("#eee8d5" . 0) ("#B4C342" . 20) ("#69CABF" . 30) ("#69B7F0" . 50) ("#DEB542" . 60) ("#F2804F" . 70) ("#F771AC" . 85) ("#eee8d5" . 100))))
+    '(highlight-tail-colors
+         (quote
+             (("#eee8d5" . 0)
+                 ("#B4C342" . 20)
+                 ("#69CABF" . 30)
+                 ("#69B7F0" . 50)
+                 ("#DEB542" . 60)
+                 ("#F2804F" . 70)
+                 ("#F771AC" . 85)
+                 ("#eee8d5" . 100))))
  '(jedi:complete-on-dot t)
  '(jedi:server-args nil)
  '(magit-diff-use-overlays nil)
@@ -61,10 +78,31 @@
  '(term-default-bg-color "#fdf6e3")
  '(term-default-fg-color "#657b83")
  '(vc-annotate-background nil)
- '(vc-annotate-color-map (quote ((20 . "#dc322f") (40 . "#c85d17") (60 . "#be730b") (80 . "#b58900") (100 . "#a58e00") (120 . "#9d9100") (140 . "#959300") (160 . "#8d9600") (180 . "#859900") (200 . "#669b32") (220 . "#579d4c") (240 . "#489e65") (260 . "#399f7e") (280 . "#2aa198") (300 . "#2898af") (320 . "#2793ba") (340 . "#268fc6") (360 . "#268bd2"))))
+    '(vc-annotate-color-map
+         (quote
+             ((20 . "#dc322f")
+                 (40 . "#c85d17")
+                 (60 . "#be730b")
+                 (80 . "#b58900")
+                 (100 . "#a58e00")
+                 (120 . "#9d9100")
+                 (140 . "#959300")
+                 (160 . "#8d9600")
+                 (180 . "#859900")
+                 (200 . "#669b32")
+                 (220 . "#579d4c")
+                 (240 . "#489e65")
+                 (260 . "#399f7e")
+                 (280 . "#2aa198")
+                 (300 . "#2898af")
+                 (320 . "#2793ba")
+                 (340 . "#268fc6")
+                 (360 . "#268bd2"))))
  '(vc-annotate-very-old-color nil)
  '(vc-command-messages t)
- '(weechat-color-list (quote (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))))
+    '(weechat-color-list
+         (quote
+             (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -106,6 +144,12 @@
     '(lambda ()
          (setq tab-width 2)
          (setq indent-tabs-mode t)))
+
+(setq jabber-account-list
+    '((:password: nil)
+         (:network-server . "")
+         (:port 5220)
+         (:connection-type . ssl)))
 
 
 (defun org-turn-on-iimage-in-org ()
