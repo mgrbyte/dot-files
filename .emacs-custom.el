4510e47cf26694bf14d86325ab79f80be05756b2
Matt Russell <matthew.russell@horizon5.org>
2015-01-26 16:08:25 +0000

    Updated to include jabber notifications.

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -12,7 +12,6 @@
 (require 'magit)
 (require 'org)
 (require 'org-install)
-;; (require 'pyvenv)
 (require 'python)
 (require 'rst)
 (require 's)
@@ -30,12 +29,9 @@
 (setq ido-file-extensions-order
       '(".py" ".zcml" ".el" ".xml" ".js"))
 
-;; org-mode config
-;; TDB move out to separate elisp file
-(setq org-log-done #'time)
-(setq org-agenda-files
-      (list "~/org/work.org"
-            "~/org/home.org"))
+;; org-mode
+(setq org-log-done t)
+(setq org-agenda-files (list "~/org/work.org"))
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
@@ -43,11 +39,14 @@
    ))
 
 (setq-default theme-load-from-file t)
+(setq-default theme-default 'solarized-dark)
 (menu-bar-mode 1)
 (set-fill-column 79)
 
 (defun setup-global-key-bindings ()
   "Setup global key bindings."
+  (require 'magit)
+  (require 'org)
   (global-set-key (kbd "C-c +") 'text-scale-increase)
   (global-set-key (kbd "C-c -") 'text-scale-decrease)
   (global-set-key (kbd "C-c l") 'org-store-link)
@@ -63,23 +62,23 @@
 
 (defvar pylons-git-repos
   (list "colander.git"
-       "deform.git"
-    "peppercorn.git"
-    "pyramid.git"
-    "pyramid_chameleon.git"
-    "pyramid_mako.git"
-    "pyramid_layout.git"
-    "pyramid_ldap"
-    "pyramid_zcml.git"
-    "pyramid_zodbconn.git"
-    "sdidemo.git"
-    "substanced.git"))
+	"deform.git"
+	"peppercorn.git"
+	"pyramid.git"
+	"pyramid_chameleon.git"
+	"pyramid_mako.git"
+	"pyramid_layout.git"
+	"pyramid_ldap"
+	"pyramid_zcml.git"
+	"pyramid_zodbconn.git"
+	"sdidemo.git"
+	"substanced.git"))
 
 (defun is-pylons-repo (url)
   "Return true if URL is a Pylons repository."
   (delq nil (mapcar (lambda (repo-suffix)
-                      (s-suffix? repo-suffix url))
-                    pylons-git-repos)))
+		      (s-suffix? repo-suffix url))
+		    pylons-git-repos)))
 
 
 (defun git-get-current-remote-name ()
@@ -92,9 +91,9 @@
 (defun py-set-flycheck-flake8rc-for-current-git-repo()
   (require 'flycheck)
   (let* ((curr-git-remote-url (git-get-current-remote-name))
-         (flake8rc-filename "flake8rc"))
+	 (flake8rc-filename "flake8rc"))
     (if (is-pylons-repo curr-git-remote-url)
-        (setq flake8rc-filename "pylons.flake8rc"))
+	(setq flake8rc-filename "pylons.flake8rc"))
     (setq-default flycheck-flake8rc (concat "~/.config/" flake8rc-filename))))
 
 (defun pyvenv-activate-safely (directory)
@@ -105,27 +104,26 @@
 (defun py-venv-known-names (directory)
   "List `known` virtualenvs names only in DIRECTORY."
   (let* ((dir-name (directory-file-name directory))
-         (full-names 1)
-         (files (directory-files directory full-names))
-         (dirs (remove-if-not #'file-directory-p files))
-         (names (remove-if
-                 #'(lambda (name)
-                     (s-match ".+\\.+$" name)) dirs)))
+	 (full-names 1)
+	 (files (directory-files directory full-names))
+	 (dirs (remove-if-not #'file-directory-p files))
+	 (names (remove-if #'(lambda (name) (s-match ".+\\.+$" name)) dirs)))
     (mapcar #'file-name-base names)))
 
 (defun py-auto-workon-maybe ()
   "Attempt to automatically workon known virtualenvs."
   (require 'pyvenv)
   (let* ((git-remote-name (git-get-current-remote-name))
-         (git-repo-name (or (file-name-base git-remote-name) ""))
-         (venv-names (py-venv-known-names py-workon-home))
-         (venvs-matched (remove-if-not
-                         #'(lambda (venv-name)
-                             (s-contains? git-repo-name venv-name))
-                         venv-names)))
+	 (git-repo-name (or (file-name-base git-remote-name) ""))
+	 (venv-names (py-venv-known-names py-workon-home))
+	 (venvs-matched
+	  (remove-if-not
+	   #'(lambda (venv-name) (s-contains? git-repo-name venv-name))
+	   venv-names)))
     (if (and (> 1 (length venvs-matched)) pyvenv-virtual-env)
-        (pyvenv-deactivate)
+	(pyvenv-deactivate)
       (pyvenv-workon (car venvs-matched)))))
+
 
 (defun py-handle-virtualenvs ()
   "Handle Python virualenvs."
@@ -149,15 +147,28 @@
 (add-hook 'rst-mode #'py-handle-sphinx-docs)
 
 (add-hook 'dired-load-hook
-          '(lambda ()
-             (require 'dired-x)
-             (dired-omit-mode 1)))
+	  '(lambda ()
+	     (dired-omit-mode 1)))
 
 (setq-default jabber-account-list
-              '((:password: nil)
-                (:network-server . "")
-                (:port 5220)
-                (:connection-type . ssl)))
+    '((:password: nil)
+      (:network-server . "")
+      (:port 5220)
+      (:connection-type . ssl)))
+(require 'notify)
 
+
+(defun notify-jabber-notify (from buf text proposed-alert)
+  "Notify via notify.el about new messages using FROM BUF TEXT PROPOSED-ALERT."
+  (when (or jabber-message-alert-same-buffer
+            (not (memq (selected-window) (get-buffer-window-list buf))))
+    (if (jabber-muc-sender-p from)
+        (notify (format "(PM) %s"
+                       (jabber-jid-displayname (jabber-jid-user from)))
+               (format "%s: %s" (jabber-jid-resource from) text)))
+      (notify (format "%s" (jabber-jid-displayname from))
+             text)))
+
+(add-hook 'jabber-alert-message-hooks 'notify-jabber-notify)
 (provide '.emacs-custom)
 ;;; .emacs-custom.el ends here
