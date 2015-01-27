3ecfe2f81a38a71b267f457dc7eb3967a4f15bb5
Matt Russell <matthew.russell@horizon5.org>
2015-01-27 20:46:05 +0000

    Fix deploy script. Remove old stuff.

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,15 @@
 # setup dot files
 ARCHIVE="/tmp/dot-files.tar.gz"
 git archive -o "$ARCHIVE" HEAD
+
 pushd "$HOME"
 tar xvf "$ARCHIVE"
 rm "$ARCHIVE"
 
-# Ensure emacs is installed properly
-cd "$HOME/.emacs.d"
-git remote -v | grep "netsight/emacs"
-if [ $? -ne 0 ]; then
-    cd "$HOME"
-    git clone https://github.com/netsight/emacs.d ~/.emacs.d
-    make
-fi
-popd
+EMACS_DOT_D="$HOME/.emacs.d"
+mv "$EMACS_DOT_D" "${EMACS_DOT_D}.bak"
+git clone https://github.com/netsight/emacs.d "$EMACS_DOT_D"
+cd "$EMACS_DOT_D"
+make
+
 
