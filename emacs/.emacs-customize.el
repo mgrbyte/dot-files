e3516cf9b927dfad382135220d2990e312b40e4c
Matt Russell <matthew.russell@horizon5.org>
2015-04-20 15:27:59 +0100

    Set faces for jabber.

--- emacs/.emacs-customize.el
+++ emacs/.emacs-customize.el
@@ -42,4 +42,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(jabber-activity-personal-face ((t (:foreground "gold" :weight bold))))
+ '(jabber-roster-user-online ((t (:foreground "cornsilk" :slant normal :weight bold)))))
