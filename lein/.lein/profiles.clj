acbd73c646a14034053a010e11562191066d5e69
Matt Russell <matt@mgrbyte.co.uk>
2016-02-16 20:12:09 +0000

    Update installation wide lein deps.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,1 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
+{:user {
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+        :dependencies [[com.datomic/datomic-free "0.9.5350"]
+                       [me.raynes/fs "1.4.6"]]
+        }}
