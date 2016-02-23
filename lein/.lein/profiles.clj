f7c454144e6ff3ff76a68a016374106d3f27dd71
Matt Russell <matt@mgrbyte.co.uk>
2016-02-23 13:43:46 +0000

    Update lein profile to include flycheck-clojoure deps.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,12 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-        :dependencies [[com.datomic/datomic-free "0.9.5350"]
-                       [me.raynes/fs "1.4.6"]]
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-kibit "0.1.2"]
+                  [lein-typed "0.3.5"]
+                  ]
+        :dependencies [[org.clojure/clojure "1.8.0"]
+                       [com.datomic/datomic-free "0.9.5350"]
+                       [me.raynes/fs "1.4.6"]
+                       ;; Consider using typed? [org.clojure/core.typed "0.3.22"]
+                       ]
         }}
