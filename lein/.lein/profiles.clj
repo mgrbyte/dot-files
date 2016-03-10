e0c9fc518c250dfe12f11edc1689f271e0009538
Matt Russell <matt@mgrbyte.co.uk>
2016-03-10 08:06:35 +0000

    Add clj-stacktrace to global lein deps.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,12 +1,14 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+        :plugins [[cider/cider-nrepl "0.11.0"]
                   [jonase/eastwood "0.2.3"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
                   ]
         :dependencies [[org.clojure/clojure "1.8.0"]
-                       [com.datomic/datomic-free "0.9.5350"]
+                       [datomic-schema-grapher "0.0.1"]
+                       [com.datomic/datomic-pro "0.9.5350"]
                        [me.raynes/fs "1.4.6"]
+                       [clj-stacktrace "0.2.8"]
                        ;; Consider using typed? [org.clojure/core.typed "0.3.22"]
                        ]
         }}
