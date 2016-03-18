93f7f8d5d919a8ade2a1c4bac04985f058ac0e4a
Matt Russell <matt@mgrbyte.co.uk>
2016-03-18 15:39:01 +0000

    Update lein profile.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,14 +1,14 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.2"]
-                  [lein-typed "0.3.5"]
-                  ]
-        :dependencies [[org.clojure/clojure "1.8.0"]
+        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+
+        :dependencies [[org.clojure/clojure "1.7.0"]
                        [datomic-schema-grapher "0.0.1"]
                        [com.datomic/datomic-pro "0.9.5350"]
                        [me.raynes/fs "1.4.6"]
                        [clj-stacktrace "0.2.8"]
+                       [acyclic/squiggly-clojure "0.1.5"]
+                       ^:replace [org.clojure/tools.nrepl "0.2.12"]
                        ;; Consider using typed? [org.clojure/core.typed "0.3.22"]
                        ]
         }}
+
