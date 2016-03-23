ef9bba031d8db1bc1e100b3e86889e5893022c53
Matt Russell <matt@mgrbyte.co.uk>
2016-03-23 10:07:49 +0000

    Add eastwood lint checker.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                  [jonase/eastwood "0.2.3"]]
 
         :dependencies [[org.clojure/clojure "1.7.0"]
                        [datomic-schema-grapher "0.0.1"]
@@ -10,5 +11,11 @@
                        ^:replace [org.clojure/tools.nrepl "0.2.12"]
                        ;; Consider using typed? [org.clojure/core.typed "0.3.22"]
                        ]
-        }}
+        }
+
+ ;; VisualVM profiling opts
+ :jvm-opts ["-Dcom.sun.management.jmxremote"
+            "-Dcom.sun.management.jmxremote.ssl=false"
+            "-Dcom.sun.management.jmxremote.authenticate=false"
+            "-Dcom.sun.management.jmxremote.port=43210"]}
 
