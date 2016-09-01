27c4069ba8f6ddd8b59066a8ce3274b45d48310b
Matt Russell <matt@mgrbyte.co.uk>
2016-09-01 12:06:11 +0100

    Fix up global deps and plugins.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,9 +1,17 @@
-{:user {
-          :dependencies [[cider/cider-nrepl "0.12.0-SNAPSHOT"] ; Emnacs
-                         [acyclic/squiggly-clojure "0.1.5"] ; Emacs
-                         ^:replace [org.clojure/tools.nrepl "0.2.12"] ; Emacs
-                         ]
-        :singing {:gpg-key "matthew.russell@wormbase.org"}}
+{:user {:plugin-repositories [["private-plugins" {:url "private repo url"}]]
+        :singing {:gpg-key "matthew.russell@wormbase.org"}
+        :dependencies [[clojure-ini "0.0.2"]
+                       [me.raynes/fs "1.4.6"]]
+        :plugins [[cider/cider-nrepl "0.8.2"]
+                  [refactor-nrepl "0.2.2"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-ancient "0.6.8"]
+                  [lein-bikeshed "0.3.0"]
+                  [lein-kibit "0.1.2"]
+                  [lein-ns-dep-graph "0.1.0-SNAPSHOT"]]}
+ :repl {:dependencies [[acyclic/squiggly-clojure "0.1.6"]
+                       [datomic-schema-grapher "0.0.1"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}
  ;; VisualVM profiling opts
  :jvm-opts ["-Dcom.sun.management.jmxremote"
             "-Dcom.sun.management.jmxremote.ssl=false"
