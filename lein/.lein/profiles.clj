9c1c32c71187f1bb2af141967ac5b7ea0c8f82db
Matt Russell <matt@mgrbyte.co.uk>
2016-11-12 09:19:33 +0000

    Do not set a user-wide :dev profile as per lein docs.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugin-repositories [["private-plugins"
+ {:env {:squiggly {:checkers [:eastwood]}}
+  :plugin-repositories [["private-plugins"
                          {:url "private repo url"}]]
         :singing {:gpg-key "matthew.russell@wormbase.org"}
         :dependencies [[clojure-ini "0.0.2"]
@@ -14,8 +15,6 @@
  :repl {:dependencies [[acyclic/squiggly-clojure "0.1.6"]
                        [datomic-schema-grapher "0.0.1"]
                        [org.clojure/tools.nrepl "0.2.12"]]}
- :dev {:env
-       {:squiggly {:checkers [:eastwood]}}}
  ;; VisualVM profiling opts
  :jvm-opts ["-Dcom.sun.management.jmxremote"
             "-Dcom.sun.management.jmxremote.ssl=false"
