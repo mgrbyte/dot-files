33e935f2a26c8f6920d05c8dbe3abc2ce5691117
Matt Russell <matt@mgrbyte.co.uk>
2017-02-01 11:12:27 +0000

    Keep emacs backup files in a central location.

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -21,7 +21,14 @@
 
 (use-package mgrbyte
   :init
-  (toggle-frame-maximized))
+  (toggle-frame-maximized)
+  :config
+  (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+	backup-by-copying t
+	version-control t
+	delete-old-versions t
+	kept-new-versions 20
+	kept-old-versions 5))
 
 
 (provide '.emacs-custom)
