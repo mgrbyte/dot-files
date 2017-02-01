d3340ffe316ac55143a46ddc9afb45335e647124
Matt Russell <matt@mgrbyte.co.uk>
2017-02-01 11:13:18 +0000

    Disabled visual-vm jvm-opts.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -10,14 +10,15 @@
                   [jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.8"]
                   [lein-bikeshed "0.3.0"]
-                  [lein-kibit "0.1.2"]
                   [lein-ns-dep-graph "0.1.0-SNAPSHOT"]]}
  :repl {:dependencies [[acyclic/squiggly-clojure "0.1.6"]
                        [datomic-schema-grapher "0.0.1"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}
+                       [org.clojure/tools.nrepl "0.2.12"]]
+        :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]]}
  :repl-options {:init (set! *print-length* 10)}
  ;; VisualVM profiling opts
- :jvm-opts ["-Dcom.sun.management.jmxremote"
-            "-Dcom.sun.management.jmxremote.ssl=false"
-            "-Dcom.sun.management.jmxremote.authenticate=false"
-            "-Dcom.sun.management.jmxremote.port=43210"]}
+ ;; :jvm-opts ["-Dcom.sun.management.jmxremote"
+ ;;            "-Dcom.sun.management.jmxremote.ssl=false"
+ ;;            "-Dcom.sun.management.jmxremote.authenticate=false"
+ ;;            "-Dcom.sun.management.jmxremote.port=43210"]}
+ }
