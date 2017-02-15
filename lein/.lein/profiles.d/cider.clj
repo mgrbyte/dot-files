a11625211798181bd0da7c4e8a3855a51b1bd176
Matt Russell <matt@mgrbyte.co.uk>
2017-02-15 17:39:36 +0000

    Mods to cider lein profile.

--- lein/.lein/profiles.d/cider.clj
+++ lein/.lein/profiles.d/cider.clj
@@ -1,11 +1,13 @@
 {:plugins [[cider/cider-nrepl "0.14.0"]
+           [jonase/eastwood "0.2.3"
+            :exclusions [org.clojure/clojure]]
            [refactor-nrepl "2.2.0"]]
- :dependencies [[cider/cider-nrepl "0.14.0"]]
+ :dependencies [[acyclic/squiggly-clojure "0.1.6"]]
  :env {:squiggly
               ;; Quote options to avoid warnings
               ;; introduced with `lein-environ 1.1.0` plugin
               "{:checkers [:eastwood]
-               :eastwood-exclude-linters [:unlimited-use]}"}}
+                :eastwood-exclude-linters [:unlimited-use :no-ns-form-found]}"}}
 
 
 
