bef13ef912dc33546906fb79e5e7327b90cc4d02
Matt Russell <matt@mgrbyte.co.uk>
2017-07-23 11:52:43 +0100

    Upgraded to latest stable cider release (+ deps).

--- lein/.lein/profiles.d/cider.clj
+++ lein/.lein/profiles.d/cider.clj
@@ -1,6 +1,6 @@
-{:plugins [[cider/cider-nrepl "0.14.0"]
-           [refactor-nrepl "2.2.0"]]
- :dependencies [[cider/cider-nrepl "0.14.0"]]
+{:plugins [[cider/cider-nrepl "0.15.0"]
+           [refactor-nrepl "2.3.1"]]
+ :dependencies [[cider/cider-nrepl "0.15.0"]]
  :env {:squiggly
               ;; Quote options to avoid warnings
               ;; introduced with `lein-environ 1.1.0` plugin
