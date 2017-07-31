6b59919623ef0e787659ca22bfe4f1e66b27ff03
Matt Russell <matt@mgrbyte.co.uk>
2017-07-31 10:19:02 +0100

    Merged update to cider 0.15.0

--- lein/.lein/profiles.d/cider.clj
+++ lein/.lein/profiles.d/cider.clj
@@ -3,10 +3,10 @@
  :dependencies [[cider/cider-nrepl "0.15.0"]
                 [acyclic/squiggly-clojure "0.1.6"]]
  :env {:squiggly
-              ;; Quote options to avoid warnings
-              ;; introduced with `lein-environ 1.1.0` plugin
-              "{:checkers [:eastwood]
-                :eastwood-exclude-linters [:unlimited-use :no-ns-form-found]}"}}
+       ;; Quote options to avoid warnings
+       ;; introduced with `lein-environ 1.1.0` plugin
+       "{:checkers [:eastwood]
+         :eastwood-exclude-linters [:unlimited-use :no-ns-form-found]}"}}
 
 
 
