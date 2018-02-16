28273909431d9d179f0d7462ffbfebe00cea5b83
Matt Russell <matt@mgrbyte.co.uk>
2018-02-16 10:01:09 +0000

    Updated deps, removed comment.

--- lein/.lein/profiles.d/cider.clj
+++ lein/.lein/profiles.d/cider.clj
@@ -1,10 +1,8 @@
-{:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
+{:plugins [[cider/cider-nrepl "0.17.0-SNAPSHOT"]
            [refactor-nrepl "2.4.0-SNAPSHOT"]]
  :dependencies [[acyclic/squiggly-clojure "0.1.8"]
-                [compliment "0.3.4"]]
+                [compliment "0.3.5"]]
  :env {:squiggly
-       ;; Quote options to avoid warnings
-       ;; introduced with `lein-environ 1.1.0` plugin
        "{:checkers [:eastwood]
          :eastwood-exclude-linters [:unlimited-use :no-ns-form-found]}"}}
 
