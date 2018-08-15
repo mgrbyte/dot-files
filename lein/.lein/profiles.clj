af020e001fc3301089d8f1dd7098adbf4ae69c0f
Matt Russell <matt@mgrbyte.co.uk>
2018-08-15 13:33:41 +0100

    Use matching verison of cider.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,8 +1,7 @@
-{:repl {:plugins [[cider/cider-nrepl "0.18.0"]
+{:repl {:plugins [[cider/cider-nrepl "0.18.0-SNAPSHOT"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]]
         :dependencies
-        [[acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
-          :exclusions [org.clojure/tools.reader]]
+        [[acyclic/squiggly-clojure "0.1.9-SNAPSHOT" :exclusions [org.clojure/tools.reader]]
          [compliment "0.3.5"]]}
  :env {:squiggly
        "{:checkers [:eastwood]
