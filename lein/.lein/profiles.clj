4dd784f413cfa50b0f702ecb4332217fd463b2e7
Matt Russell <matt@mgrbyte.co.uk>
2018-08-15 11:56:28 +0100

    Re-instate lein profile for cider.

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,10 @@
+{:repl {:plugins [[cider/cider-nrepl "0.18.0"]
+                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
+        :dependencies
+        [[acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
+          :exclusions [org.clojure/tools.reader]]
+         [compliment "0.3.5"]]}
+ :env {:squiggly
+       "{:checkers [:eastwood]
+         :eastwood-exclude-linters [:unlimited-use :no-ns-form-found]}"}}
+
